b438a2d680a0bc1f27672212cfd75d698e05efb8
amrut <noamrut@gmail.com>
2026-03-02 12:28:49 +0530

    added basic in memory session

---
+++ cookies/basicWithSession/server.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const crypto = require("crypto");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const users = [{
+    id: 1,
+    name: "amrut",
+    password: "123"
+},
+{
+    id: 2,
+    name: "pooja",
+    password: "1234"
+}];
+
+const sessions = {};
+app.use(express.json());
+app.use(cookieParser());
+
+const authenticate = (req, res, next) =>{
+    const sessionId = req.cookies.sessionId;
+    if(!sessionId || !sessions[sessionId]){
+        return res.status(401).send("Invalid credentials");
+    }
+    req.user = users.find(u => u.id === sessions[sessionId].id);
+    next();
+}; 
+
+app.get("/", (req, res) =>{
+    const sessionId = req.cookies.sessionId;
+    if(!sessionId || !sessions[sessionId]){
+        return res.send("please login first");
+    };
+    const userid = sessions[sessionId].id
+    const user = users.find(u => u.id === userid);
+    res.send(`Welcome Back, ${user.name || "user"}`);
+});
+
+app.get("/dashboard", authenticate, (req, res) =>{
+    res.send(`welcome to dashboard, ${req.user.name}`);
+});
+
+app.post("/login", (req, res) =>{
+    const {username, password} = req.body;
+    if(!username || !password){
+        return res.status(401).send("please add credentials first");
+    }
+
+    const user = users.find(u => u.name === username && u.password === password);
+    if (user){
+        const sessionId = crypto.randomBytes(16).toString("hex");
+        sessions[sessionId] = {
+            id: user.id,
+            startTime: Date.now()
+        }
+
+        res.cookie("sessionId", sessionId, {
+            httpOnly: true,
+            maxAge: 60000
+        });
+
+        console.log(`${username} has logged in`);
+        res.redirect("/")
+    }
+});
+
+app.get("/logout", (req, res) =>{
+    const sessionId = req.cookies.sessionId;
+    if(sessionId){
+        delete sessions[sessionId];
+    }
+    res.clearCookie("sessionId");
+    res.send("logged out successfully")
+});
+
+app.listen(PORT, () =>{
+    console.log("server is running at port", PORT);
+});
